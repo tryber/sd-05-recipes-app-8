7154f150d453de45de539b4c84720ec4732ff1a1
Douglas <douglaza@gmail.com>
2020-09-09 01:39:26 -0300

    login skeleton - 4th try

--- src/components/BackToLogin.jsx
+++ src/components/BackToLogin.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BackToLogin = () => {
-  return (
-    <Link to="/">
-      <button>Voltar ao Início</button>
-    </Link>
-  );
-};
+const BackToLogin = () => (
+  <Link to="/">
+    <button>Voltar ao Início</button>
+  </Link>
+);
 
 export default BackToLogin;
--- src/components/BtnLogin.jsx
+++ src/components/BtnLogin.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const BtnLogin = () => {
-  const checkLogin = () => {
-    return false;
-  };
+  const checkLogin = () => false;
 
   return (
     <Link to="/comidas">
