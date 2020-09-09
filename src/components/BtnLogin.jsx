7d13665014216fedeb4063c00bc5971693d06a11
Douglas <douglaza@gmail.com>
2020-09-09 12:46:23 -0300

    setting on Storage

--- src/components/BtnLogin.jsx
+++ src/components/BtnLogin.jsx
@@ -6,12 +6,18 @@ const BtnLogin = () => {
   const { email, password } = useContext(RecipesContext);
   const checkPassword = (keyword) => !(keyword.length < 6);
   const checkEmail = (mail) => mail.match(/\S+@\S+\.\S+/);
+  const setStorage = () => {
+    localStorage.setItem('mealsToken', '1');
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email: email }));
+  };
   return (
     <Link to="/comidas">
       <button
         data-testid="login-submit-btn"
         type="button"
         disabled={!(checkPassword(password) && checkEmail(email))}
+        onClick={() => setStorage()}
       >
         Entrar
       </button>
