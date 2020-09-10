0dbfa01da9a99c6e6e47ec553a98cfc279ecea13
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-09-10 14:19:35 -0300

    corrigindo estilo

--- src/components/MenuBottom.jsx
+++ src/components/MenuBottom.jsx
@@ -5,35 +5,33 @@ import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 
-const MenuBottom = () => {
-  return (
-    <footer className="menu-bottom" data-testid="footer">
-      <Link to="/bebidas">
-        <img
-          src={drinkIcon}
-          alt="Drinks"
-          className="drink-icon icon-bottom"
-          data-testid="drinks-bottom-btn"
-        />
-      </Link>
-      <Link to="/explorar">
-        <img
-          src={exploreIcon}
-          alt="Explore"
-          className="explore-icon icon-bottom"
-          data-testid="explore-bottom-btn"
-        />
-      </Link>
-      <Link to="/comidas">
-        <img
-          src={mealIcon}
-          alt="Meals"
-          className="meal-icon icon-bottom"
-          data-testid="food-bottom-btn"
-        />
-      </Link>
-    </footer>
-  );
-};
+const MenuBottom = () => (
+  <footer className="menu-bottom" data-testid="footer">
+    <Link to="/bebidas">
+      <img
+        src={drinkIcon}
+        alt="Drinks"
+        className="drink-icon icon-bottom"
+        data-testid="drinks-bottom-btn"
+      />
+    </Link>
+    <Link to="/explorar">
+      <img
+        src={exploreIcon}
+        alt="Explore"
+        className="explore-icon icon-bottom"
+        data-testid="explore-bottom-btn"
+      />
+    </Link>
+    <Link to="/comidas">
+      <img
+        src={mealIcon}
+        alt="Meals"
+        className="meal-icon icon-bottom"
+        data-testid="food-bottom-btn"
+      />
+    </Link>
+  </footer>
+);
 
 export default MenuBottom;
