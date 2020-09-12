737ea2f3ea8e721a3e4ba86bd1236198df38b2db
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-09-12 17:29:32 -0300

    Corrigindo CC

--- src/services/mealAPI.js
+++ src/services/mealAPI.js
@@ -1,23 +1,12 @@
 export const mealApi = 'https://www.themealdb.com/api/json/v1/1/';
+const mealCategories =
+  'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
-export const fetchMealData = (search) => {
-  const searchString = `${mealApi}${search}`;
-  return fetch(searchString).then((response) =>
-    response
-      .json()
-      .then((json) =>
-        response.ok ? Promise.resolve(json) : Promise.reject(json),
-      ),
+const fetchData = (api) =>
+  fetch(api).then((response) =>
+    response.json().then((json) => (
+      response.ok ? Promise.resolve(json) : Promise.reject(json)
+    )),
   );
-};
 
-export const fetchCategories = () =>
-  fetch(
-    'https://www.themealdb.com/api/json/v1/1/list.php?c=list',
-  ).then((response) =>
-    response
-      .json()
-      .then((json) =>
-        response.ok ? Promise.resolve(json) : Promise.reject(json),
-      ),
-  );
+export const fetchCategories = () => fetchData(mealCategories);
