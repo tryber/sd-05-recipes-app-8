00b1d9fdae09ea41c98101b732deca704dcfb87a
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-09-15 01:52:24 -0300

    forçando merge

--- src/services/getRecipeDetails.jsx
+++
@@ -1,8 +0,0 @@
-const getRecipeDetails = (type, id) => {
-  const RECIPE_API = `https://www.the${type}db.com/api/json/v1/1/lookup.php?i=${id}`;
-  return fetch(RECIPE_API).then((response) =>
-    response.json().then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json))),
-  );
-};
-
-export default getRecipeDetails;
