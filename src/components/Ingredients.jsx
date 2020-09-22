87cbc23619c025f4a1fe9b534e7b6606672d62d9
Douglas <douglaza@gmail.com>
2020-09-22 00:54:12 -0300

    recipes details fixed - 2nd try

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Ingredients = (props) => {
-  const ingredientsList = (ingredientsRecipes) => (
+  const menu =
+    props.keys[1] === 'meals'
+      ? {
+        item: Object.values(props.recipe[props.keys[1]][0]).slice(9, 29),
+        quantity: Object.values(props.recipe[props.keys[1]][0]).slice(29, 49),
+      }
+      : {
+        item: Object.values(props.recipe[props.keys[1]][0]).slice(21, 36),
+        quantity: Object.values(props.recipe[props.keys[1]][0]).slice(36, 51),
+      };
+
+  const merged = menu.item.map((value, i) => [value, menu.quantity[i]]);
+
+  return (
     <div>
       <h4>Ingredients</h4>
       <ul>
-        {ingredientsRecipes.map(
+        {merged.map(
           (ingredient, index) =>
             ingredient[0] && (
               <li
@@ -24,21 +37,12 @@ const Ingredients = (props) => {
       </ul>
     </div>
   );
-  const menu =
-    props.keys[1] === 'meals'
-      ? {
-          item: Object.values(props.recipe[props.keys[1]][0]).slice(9, 29),
-          quantity: Object.values(props.recipe[props.keys[1]][0]).slice(29, 49),
-        }
-      : {
-          item: Object.values(props.recipe[props.keys[1]][0]).slice(21, 36),
-          quantity: Object.values(props.recipe[props.keys[1]][0]).slice(36, 51),
-        };
-
-  const merged = menu.item.map((value, i) => [value, menu.quantity[i]]);
-  return ingredientsList(merged);
 };
 
-Ingredients.propTypes = { itemId: PropTypes.string.isRequired };
-
 export default Ingredients;
+
+Ingredients.propTypes = {
+  itemId: PropTypes.string.isRequired,
+  keys: PropTypes.arrayOf(PropTypes.string).isRequired,
+  recipe: PropTypes.node.isRequired,
+};
