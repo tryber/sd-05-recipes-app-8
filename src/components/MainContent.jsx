6de9747cc06e9b83356e1154f8322f1fc812cec7
Douglas <douglaza@gmail.com>
2020-09-27 19:16:48 -0300

    timeless

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -7,18 +7,13 @@ const MainContent = () => {
   const { recipesRoster, categorySelected } = useContext(RecipesContext);
   return (
     <div className="recipes-card-container">
+      {!recipesRoster[0] &&
+        categorySelected !== '' &&
+        alert('Sinto muito, não encontramos nenhuma receita para esses filtros.')}
       {recipesRoster.length >= 1 &&
         recipesRoster.map((recipe, index) => (
-          <MainCard
-            cardData={recipe}
-            index={index.toString()}
-            key={index.toString()}
-          />
+          <MainCard cardData={recipe} index={index.toString()} key={index.toString()} />
         ))}
-      {recipesRoster.length === 0 && categorySelected !== '' &&
-        alert(
-          'Sinto muito, não encontramos nenhuma receita para esses filtros.',
-        )}
     </div>
   );
 };
